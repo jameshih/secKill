4e1310aadddf6b15ab4cbb1e04de0e71a1378c15
jameshih <j@shih.app>
2020-05-27 21:22:01 -0400

    fix vvariable recursion call

--- admin/main/init.go
+++ admin/main/init.go
@@ -11,24 +11,19 @@ import (
 	"go.etcd.io/etcd/clientv3"
 )
 
-var (
-	Db         *sqlx.DB
-	EtcdClient *clientv3.Client
-)
-
-func initDB() (err error) {
+func initDB() (db *sqlx.DB, err error) {
 	dns := fmt.Sprintf("%s:%s@tcp(%s:%d)/%s", AppConf.mysqlConf.Username, AppConf.mysqlConf.Password, AppConf.mysqlConf.Host, AppConf.mysqlConf.Port, AppConf.mysqlConf.Database)
 	database, err := sqlx.Open("mysql", dns)
 	if err != nil {
 		logs.Error("failed to connect to mysql, err: %v", err)
 		return
 	}
-	Db = database
+	db = database
 	logs.Debug("init MysqlDB succ")
 	return
 }
 
-func initEtcd() (err error) {
+func initEtcd() (etcdClient *clientv3.Client, err error) {
 	cli, err := clientv3.New(clientv3.Config{
 		Endpoints:   []string{AppConf.etcdConf.Addr},
 		DialTimeout: time.Duration(AppConf.etcdConf.EtcdTimeout) * time.Second,
@@ -38,7 +33,7 @@ func initEtcd() (err error) {
 		return
 	}
 
-	EtcdClient = cli
+	etcdClient = cli
 	logs.Debug("init Etcd succ")
 	return
 }
@@ -49,19 +44,19 @@ func initAll() (err error) {
 		logs.Warn("init config failed, error: %v", err)
 		return
 	}
-	err = initDB()
+	db, err := initDB()
 	if err != nil {
 		logs.Warn("init db failed, error: %v", err)
 		return
 	}
 
-	err = initEtcd()
+	etcdClient, err := initEtcd()
 	if err != nil {
 		logs.Warn("init etcd failed, error: %v", err)
 		return
 	}
 
-	err = model.Init(Db)
+	err = model.Init(db, etcdClient)
 	if err != nil {
 		logs.Warn("init model failed, error: %v", err)
 		return
--- admin/model/db.go
+++ admin/model/db.go
@@ -1,12 +1,19 @@
 package model
 
-import "github.com/jmoiron/sqlx"
-
-var (
-	Db *sqlx.DB
+import (
+	"github.com/astaxie/beego/logs"
+	"github.com/jmoiron/sqlx"
+	"go.etcd.io/etcd/clientv3"
 )
 
-func Init(db *sqlx.DB) (err error) {
+var (
+	Db         *sqlx.DB
+	EtcdClient *clientv3.Client
+)
+
+func Init(db *sqlx.DB, etcdClient *clientv3.Client) (err error) {
 	Db = db
+	EtcdClient = etcdClient
+	logs.Debug("init all succ")
 	return
 }
